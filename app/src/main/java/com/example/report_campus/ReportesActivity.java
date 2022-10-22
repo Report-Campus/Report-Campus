5ff69bab7dd760560438c75a81c4a99d2221315c
Maria Eduarda <eduardalbuquerquecox@gmail.com>
2022-10-22 14:17:41 -0300

    tela reports

---
+++ app/src/main/java/com/example/report_campus/ReportesActivity.java
@@ -0,0 +1,14 @@
+package com.example.report_campus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportes);
+    }
+}
