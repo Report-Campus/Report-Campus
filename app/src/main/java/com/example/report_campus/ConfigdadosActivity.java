8336e9efe8a5a63823ddab6a1014f0750cf62b5b
Maria Eduarda <eduardalbuquerquecox@gmail.com>
2022-10-22 21:39:41 -0300

    telas config

---
+++ app/src/main/java/com/example/report_campus/ConfigdadosActivity.java
@@ -0,0 +1,14 @@
+package com.example.report_campus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigdadosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configdados);
+    }
+}
---
+++ app/src/main/java/com/example/report_campus/ConfigsupActivity.java
@@ -0,0 +1,14 @@
+package com.example.report_campus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigsupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configsup);
+    }
+}
