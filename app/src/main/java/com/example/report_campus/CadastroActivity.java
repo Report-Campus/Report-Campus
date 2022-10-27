d98ffef8dc341b8b2ed47f413ccd6b4705af213c
Pedro Henrique Pereira <pedrohp36@gmail.com>
2022-10-27 19:03:00 -0300

    Atualizando o autenticador

--- app/src/main/java/com/example/report_campus/CadastroActivity.java
+++ app/src/main/java/com/example/report_campus/CadastroActivity.java
@@ -5,22 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CadastroActivity extends AppCompatActivity {
 
     private EditText edt_Nome, edt_Email, edt_Senha, edt_NomeFaculdade, edt_ConfirmarSenha;
     private ImageButton bt_Cadastrar;
     String[] mensagens = {"Preencha todos os campos", "As senhas não coincidem"};
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +55,14 @@ public class CadastroActivity extends AppCompatActivity {
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
-                } else if(!confirmarSenha.equals(senha) && !senha.equals(confirmarSenha)){
+                } else if (!confirmarSenha.equals(senha) && !senha.equals(confirmarSenha)){
                     Snackbar snackbar = Snackbar.make(v, mensagens[2], Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
                 } else {
                     conexao.cadastrarUsuario(v, email, senha);
+                    salvarDadosUsuario(nome, faculdade);
                 }
             }
         });
@@ -65,6 +75,29 @@ public class CadastroActivity extends AppCompatActivity {
         edt_ConfirmarSenha = findViewById(R.id.edtConfirmarSenha);
         edt_NomeFaculdade = findViewById(R.id.edtNomeFaculdade);
         bt_Cadastrar = findViewById(R.id.btCadastrar);
+    }
+
+    public void salvarDadosUsuario(String nome, String faculdade){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome", nome);
+        usuarios.put("faculdade", faculdade);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuário").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db", "Sucesso ao salvar os dados");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error", "Erro ao salvar os dados" + e.toString());
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/report_campus/ConnectionDB.java
+++ app/src/main/java/com/example/report_campus/ConnectionDB.java
@@ -2,17 +2,31 @@ package com.example.report_campus;
 
 
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionDB {
+
+    String usuarioID;
+
     public void cadastrarUsuario(View v, String email, String senha){
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -24,7 +38,20 @@ public class ConnectionDB {
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
                 } else {
-                    Snackbar snackbar = Snackbar.make(v, "Erro no cadastro", Snackbar.LENGTH_SHORT);
+                    String erro;
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Digite uma senha com no mínimo 6 caracteres";
+                    } catch (FirebaseAuthUserCollisionException e) {
+                        erro = "Está conta já exxiste";
+                    } catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "E-mail inválido";
+                    } catch (Exception e){
+                        erro = "Erro ao cadastrar o usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
@@ -32,5 +59,9 @@ public class ConnectionDB {
             }
         });
     }
-    }
+
+
+
+}
+
 
